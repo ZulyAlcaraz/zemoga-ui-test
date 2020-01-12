a1499e57344f7413d37f65f5ba79416a6f3d7a5a
Zuly Alcaraz <zuly.alcaraz@chronwell.com>
2020-01-11 21:09:27 -0500

    add interaction

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Card } from './card.model';
+import { VotesService } from 'src/app/core/votes.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'zmg-card',
@@ -9,11 +11,33 @@ import { Card } from './card.model';
 })
 export class CardComponent implements OnInit {
 
+  thumb: boolean;
+  showVotes = true;
+  votesForm: FormGroup;
+
+  @Input()
+  index: number;
+
   @Input()
   cardInfo: Card;
 
-  constructor() { }
+  constructor(private readonly votesService: VotesService) { }
 
   ngOnInit() {
+    this.votesForm = new FormGroup({
+      thumb: new FormControl('')
+    });
+  }
+
+  changeThumb(value: boolean) {
+    this.thumb = value;
+  }
+
+  submit() {
+    this.votesService.thumbs(this.index, this.thumb);
+    this.votesForm.get('thumb').patchValue('');
+    this.votesForm.markAsPristine();
+    this.votesForm.markAsUntouched();
+    this.showVotes = false;
   }
 }
